238ada25bbbdea38507b04341097afca7149dc48
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-08-15 18:31:01 -0400

    Added the Average method without letter grade assignments

--- Exercise 2a/Program.cs
+++ Exercise 2a/Program.cs
@@ -14,7 +14,7 @@ namespace Exercise_2a
         }
         private static void Looper()
         {
-            Console.WriteLine("Welcome to exercise 2A Calculating averages.");
+            Console.WriteLine("Welcome to exercise 2A Calculating averages."); // Tried some interesting things with a menu system here.
             Console.WriteLine("Type number for process you want:");
             Console.WriteLine("1 Sum of ten numbers.");
             string process = Console.ReadLine();
@@ -24,7 +24,7 @@ namespace Exercise_2a
             else Console.WriteLine("Bad input");
             Looper(); // Fixed it to be a loop
         }
-            private static void Add() // Made this into a method to clean it up a bit.
+        private static void Add() // Made this into a method to clean it up a bit.
         {
             Console.WriteLine("Enter 10 numbers to add..");
             Console.WriteLine("Enter The first number:");
@@ -70,6 +70,53 @@ namespace Exercise_2a
             int k = (a + b + c + d + e + f + g + h + i + j);
             Console.WriteLine("The sum of those ten numbers is " + (k));
             Console.ReadLine();
+        }
+        private static void Averages()
+        {
+            Console.WriteLine("Enter the ten numbers to average");
+            Console.WriteLine("Enter the first score:");
+            string input = Console.ReadLine();
+            double a = double.Parse(input);
+
+            Console.WriteLine("Enter the second score:");
+            input = Console.ReadLine();
+            double b = double.Parse(input);
+
+            Console.WriteLine("Enter the third score:");
+            input = Console.ReadLine();
+            double c = double.Parse(input);
+
+            Console.WriteLine("Enter the fourth score:");
+            input = Console.ReadLine();
+            double d = double.Parse(input);
+
+            Console.WriteLine("Enter the fifth score:");
+            input = Console.ReadLine();
+            double e = double.Parse(input);
+
+            Console.WriteLine("Enter the sixth score:");
+            input = Console.ReadLine();
+            double f = double.Parse(input);
+
+            Console.WriteLine("Enter the seventh score:");
+            input = Console.ReadLine();
+            double g = double.Parse(input);
+
+            Console.WriteLine("Enter the eighth score:");
+            input = Console.ReadLine();
+            double h = double.Parse(input);
+
+            Console.WriteLine("Enter the ninth score:");
+            input = Console.ReadLine();
+            double i = double.Parse(input);
+
+            Console.WriteLine("Enter the final score:");
+            input = Console.ReadLine();
+            double j = double.Parse(input);
+
+            double k = ((a + b + c + d + e + f + g + h + i + j) / 10);
+            Console.WriteLine("The average score is " + k); // This is the average grade but it needs to be converted into a letter still.
+            
         }
     }
 }
